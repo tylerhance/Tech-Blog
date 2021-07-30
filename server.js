e534f1d309484fdde94f3f44ecd560c47267b762
Tyler Hance <tyler.hance@gmail.com>
2021-07-30 16:16:54 -0700

    Adding logic into the server.js file.

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('./express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('./express-handlebars');
+const hbs = exphbs.create({ helpers});
+const session = require('./express-session');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'super secret secret',
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+      },
+      resave: false,
+      saveUninitialized: true,
+      store: new SequelizeStore({
+        db: sequelize
+      })
+    };
+    
+    app.use(session(sess));
+    app.use(compression());
+    
+    // Inform Express.js on which template engine to use
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    app.use(routes);
+    
+    sequelize.sync({ force: false }).then(() => {
+      app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+    });
