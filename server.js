acf70b1c9a73d6d13e1fdc71f1caf79f6fdc82a5
Tyler Hance <tyler.hance@gmail.com>
2021-08-23 22:15:10 -0700

    Fixing heroku deployment error.

--- server.js
+++ server.js
@@ -1,46 +1,75 @@
+// const express = require('express');
+// const routes = require('./controllers');
+// const path = require('path');
+// const helpers = require('./utils/helpers');
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({ helpers });
+// const session = require('express-session');
+// const sequelize = require('./config/connection');
+
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//     secret: 'super secret secret',
+//     cookie: {
+//         maxAge: 60 * 60 * 1000,
+//         // httpOnly: true,
+//         // secure: false,
+//         // sameSite: 'strict',
+//       },
+//       resave: false,
+//       saveUninitialized: true,
+//       store: new SequelizeStore({
+//         db: sequelize
+//       })
+//     };
+    
+//     app.use(session(sess));
+    
+//     app.engine('handlebars', hbs.engine);
+//     app.set('view engine', 'handlebars');
+
+    
+//     app.use(express.json());
+//     app.use(express.urlencoded({ extended: true }));
+//     app.use(express.static(path.join(__dirname, 'public')));
+    
+//     app.use(routes);
+    
+//     sequelize.sync({ force: false }).then(() => {
+//       app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+//     });
+  
 const express = require('express');
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const path = require('path');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
-const sequelize = require('./config/connection');
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
-    secret: 'super secret secret',
-    cookie: {
-        maxAge: 60 * 60 * 1000,
-        // httpOnly: true,
-        // secure: false,
-        // sameSite: 'strict',
-      },
-      resave: false,
-      saveUninitialized: true,
-      store: new SequelizeStore({
+    secret: 'Super secret secret',
+    cookie: { maxAge: 36000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
         db: sequelize
-      })
-    };
-    
-    app.use(session(sess));
-    
-    app.engine('handlebars', hbs.engine);
-    app.set('view engine', 'handlebars');
-
-    
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.static(path.join(__dirname, 'public')));
-    
-    app.use(routes);
-    
-    sequelize.sync({ force: false }).then(() => {
-      app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
-    });
-  
-   
+    })
+};
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});  
